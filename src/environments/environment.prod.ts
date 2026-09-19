0a78eb5270e7d557523a0013913677c6efeff0fb
ranjan <ranjan.rt@assettl.com>
2026-09-19 18:39:41 +0530

    studen datails code change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1/students',
+  apiUrl: 'http://40.192.39.53:5000/api/v1/students',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/v1/students',
+  // apiUrl: 'http://localhost:5000/api/v1/students',
+    apiUrl: 'http://40.192.39.53:5000/api/v1/students',
+
 };
